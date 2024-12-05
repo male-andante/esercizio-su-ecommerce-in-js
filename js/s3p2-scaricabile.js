d30233ff310401c94f1a514c68c384b3921f50bd
male-andante <andreadanteprivitera@gmail.com>
2024-12-05 11:27:53 +0100

    completo

--- js/s3p2-scaricabile.js
+++ js/s3p2-scaricabile.js
@@ -18,7 +18,7 @@
 // ok ESERCIZIO 8: Scrivi una funzione per cambiare il colore di background della pagina
 // ok ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel footer in un altro, fittizio
 // ok ESERCIZIO 10: Scrivi una funzione per aggiungere una classe CSS ad ogni link Amazon della tabella
-// ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
-// ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto in uno differente, ogni volta che viene invocata
+// ok ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
+// ok/2 ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto in uno differente, ogni volta che viene invocata
 
 
